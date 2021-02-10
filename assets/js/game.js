8ab0ad8be72e1c6190313d98837f7ae7bd88b587
Graham Hall <ghall89@me.com>
2021-02-10 15:49:46 -0500

    added high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -87,6 +87,20 @@ var startGame = function () {
 }
 
 var endGame = function () {
+    window.alert("The game has now ended. Let's see how you did!");
+
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has a high score of " + playerInfo.money + "!");
+    } else {
+        alert(playerInfo.name + " did not bead the high scroe of " + highScore + ". Maybe next time!");
+    }
     // if player is still alive thne player wins!
     if (playerInfo.health > 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
